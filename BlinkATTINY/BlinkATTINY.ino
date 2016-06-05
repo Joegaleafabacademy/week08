d7fe8667ac02f0bec031fe7e9479a6aed9b9a2be
Joe Galea <joseph.galea@iaac.net>
2016-06-06 00:28:38 +0200

    electronic programming

---
+++ BlinkATTINY/BlinkATTINY.ino
@@ -0,0 +1,46 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the Uno and
+  Leonardo, it is attached to digital pin 13. If you're unsure what
+  pin the on-board LED is connected to on your Arduino model, check
+  the documentation at http://www.arduino.cc
+
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+ */
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(7, OUTPUT);
+  pinMode(3, INPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+ // read the state of the pushbutton value:
+  boolean buttonState = digitalRead(3); 
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on and off repeatedly:
+    digitalWrite(7, HIGH);
+    delay(50);
+    digitalWrite(7, LOW);
+    delay(50);
+     
+   
+  } else {
+    // turn LED off:
+    digitalWrite(7, LOW);
+    delay(1000);
+  }
+   }
+
+ 
